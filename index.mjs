74bf245e6c25cf1e6ebabff0c0832cd06804e511
Ynewtime <chanfoc@outlook.com>
2022-10-09 23:03:08 +0300

    v1.0
    1. Support types
    2. Backward compat
    3. Full test
    4. Auto del log file
    5. Support vr
    6. Opt bill & order format
    7. Support USB

--- index.mjs
+++ index.mjs
@@ -1,35 +1,173 @@
-// import USB from 'escpos-usb'
-import { print } from './src/index.mjs'
+import USB from './lib/escpos-usb.js'
+import print from './src/print.mjs'
+import { log, toHex, buildBill, buildOrder } from './src/utils.mjs'
 
-const bill = `^^^RECEIPT
+import net from 'node:net'
+import express from 'express'
+import cors from 'cors'
 
-${new Date().toLocaleString('zh')}
+const { findPrinter } = USB
 
--
-|品名 | 数量 | 单价 | 总价|
--
-|苹果 |
-|| 1 | 1.00 | "1.00|
-|香蕉 |
-|| 2 | 2.00 | "4.00|
--
-^TOTAL | "^5.00
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-{code:1234567890; option:code128,2,72,nohri}`
-await print(bill, `-d 192.168.1.4 -l zh`)
+/**
+ * Print main function
+ * @param {{ body: { toPrintBillContent: (import('./src/utils.mjs').ToPrintBillContent)[]; toPrintOrderContent: (import('./src/utils.mjs').ToPrintOrderContent)[] } }} req
+ * @param {*} res
+ * @returns
+ */
+async function Print(req, res) {
+  const printers = findPrinter()
+  const hasUsbPrinters = !!printers.length
+  /** @type {string[]} */
+  const errList = []
+  /** @type {string[]} */
+  const successList = []
 
-// USB打印-已调通
-// const commands = await print(bill, `-l zh`)
-// const device = new USB()
-// device.open(() => {
-//   device.write(Buffer.from(commands, 'binary'))
-//   device.close()
-// })
+  if (!('toPrintBillContent' in req.body) && !('toPrintOrderContent' in req.body)) {
+    const resMsg = `Failed: 'toPrintBillContent' and 'toPrintOrderContent' not in the body.`
+    errList.push(resMsg)
+    res.json({
+      resCode: '1',
+      resMsg,
+    })
+  }
 
-// 条形码-已调通
-// const barcode = `{code:1234567890; option:code128,2,72,nohri}`
-// await print(barcode, `-d 192.168.1.4`)
+  const { toPrintBillContent, toPrintOrderContent } = req.body
+  if (!(toPrintBillContent && toPrintBillContent.length) && !(toPrintOrderContent && toPrintOrderContent.length)) {
+    const resMsg = `'toPrintBillContent' and 'toPrintOrderContent' empty.`
+    errList.push(resMsg)
+    res.json({
+      resCode: '0',
+      resMsg,
+    })
+  }
 
-// 二维码-暂未调通
-// const qrcode = `{code:https://receiptline.github.io/designer/; option:qrcode}`
-// await print(qrcode, `-d 192.168.1.4`)
+  if (toPrintBillContent && toPrintBillContent.length) {
+    for (const record of toPrintBillContent) {
+      try {
+        const { hardwareType, ip, vid, pid, customerContent } = record
+        const { isDelivery, takeawayNo } = customerContent
+        const isTakeaway = !!takeawayNo
+        const billType = isDelivery ? 'Delivery' : isTakeaway ? 'Takeaway' : 'Onsite'
+        const billInfo = customerContent.statementID || customerContent.tableCode || customerContent.takeawayNo || customerContent.receiverName
+        if (hardwareType === 'Network') {
+          if (!ip) return errList.push(`'ip' empty.`)
+          if (net.isIP(ip) !== 4) return errList.push(`'ip: ${ip}' incorrect, should be IPv4 format like: '1.1.1.1'.`)
+          await print(buildBill(customerContent), `-d ${ip} -l zh`)
+          successList.push(`Print bill:${billType}|${billInfo} to Network:${ip} done.`)
+        } else if (hardwareType === 'USB') {
+          if (!hasUsbPrinters) {
+            errList.push(`USB Printers Not Found`)
+          } else {
+            const commands = await print(buildBill(customerContent), `-l zh`)
+            const device = new USB(vid, pid)
+            device.open((err) => {
+              if (err) {
+                errList.push(err)
+              } else {
+                device.write(Buffer.from(commands, 'binary'), device.close)
+              }
+            })
+            successList.push(`Print bill:${billType}|${billInfo} to USB:${vid}|${pid} done.`)
+          }
+        } else {
+          errList.push(`Unsupported hardwareType: ${hardwareType}`)
+        }
+      } catch (err) {
+        errList.push(err)
+      }
+    }
+  }
+
+  if (toPrintOrderContent && toPrintOrderContent.length) {
+    for (const record of toPrintOrderContent) {
+      try {
+        const { hardwareType, ip, vid, pid, chefContent } = record
+        if (hardwareType === 'Network') {
+          if (!ip) return errList.push(`'ip' empty.`)
+          if (net.isIP(ip) !== 4) return errList.push(`'ip: ${ip}' incorrect, should be IPv4 format like: '1.1.1.1'.`)
+          for (const orderCustomContent of chefContent) {
+            const orderInfo = `${orderCustomContent.food.name} x ${orderCustomContent.food.num}`
+            await print(buildOrder(orderCustomContent), `-d ${ip} -l zh`)
+            successList.push(`Print order:${orderInfo} to Network:${ip} done.`)
+          }
+        } else if (hardwareType === 'USB') {
+          if (!hasUsbPrinters) {
+            errList.push(`USB Printers Not Found`)
+          } else {
+            for (const orderCustomContent of chefContent) {
+              const commands = await print(buildOrder(orderCustomContent), `-l zh`)
+              const device = new USB(vid, pid)
+              device.open((err) => {
+                if (err) {
+                  errList.push(err)
+                } else {
+                  device.write(Buffer.from(commands, 'binary'), device.close)
+                }
+              })
+              const orderInfo = `${orderCustomContent.food.name} x ${orderCustomContent.food.num}`
+              successList.push(`Print order:${orderInfo} to USB:${vid}|${pid} done.`)
+            }
+          }
+        } else {
+          errList.push(`Unsupported hardwareType: ${hardwareType}`)
+        }
+      } catch (err) {
+        errList.push(err)
+      }
+    }
+  }
+
+  if (errList.length) {
+    log(`req.body|${JSON.stringify(req.body)}`, {
+      prefix: '[INFO]',
+      skip: true,
+    })
+    errList.forEach((err) => {
+      log(err, {
+        prefix: '[ERROR]',
+      })
+    })
+  }
+  if (successList.length) {
+    successList.forEach((success) => {
+      log(success, {
+        prefix: '[SUCCESS]',
+      })
+    })
+    res.json({
+      resCode: '0',
+      resMsg: `Print success.`,
+    })
+  } else {
+    res.json({
+      resCode: '1',
+      resMsg: `Print failed.`,
+    })
+  }
+}
+
+app.post('/print', Print)
+app.listen(2000, () => {
+  log(`////////// Printer Tool Start //////////`)
+
+  // Print out USB printers - 寻找本地的USB打印机
+  const printers = findPrinter()
+  if (!printers.length) {
+    log(`USB Printers Not Found`, {
+      prefix: '[INFO]',
+      skip: true,
+    })
+  } else {
+    log(`${printers.length} USB Printers Found`, {
+      prefix: '[INFO]',
+      details: JSON.stringify(printers),
+      skip: true,
+    })
+    printers.forEach(({ deviceDescriptor: { idVendor: vid, idProduct: pid } }, i) => log(`vid:0x${toHex(vid)}|pid:0x${toHex(pid)}`, { prefix: `[USB Printer ${i + 1}]` }))
+  }
+})
---
+++ src/utils.mjs
@@ -0,0 +1,240 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import numeral from 'numeral'
+
+import { PRICE } from './constants.mjs'
+
+/**
+ * 获取某个文件的大小（mb）
+ * https://stackoverflow.com/questions/42363140/how-to-find-the-size-of-the-file-in-node-js
+ * @param {string} fileName 文件名
+ */
+export function getFileSizeInMegaBytes(fileName) {
+  if (fs.existsSync(fileName)) {
+    const stats = fs.statSync(fileName)
+    const fileSizeInBytes = stats['size']
+    const fileSizeInMegabytes = fileSizeInBytes / 1000000.0
+    return fileSizeInMegabytes
+  }
+  return 0
+}
+
+/**
+ * 文件大小大于 1MB 则删除该文件
+ * @param {string} fileName 文件名
+ * @param {number} [megaBytes] 文件大小，默认 1 MB
+ */
+export function unlinkFileIfSizeOver(fileName, megaBytes = 1) {
+  const fileSizeInMegabytes = getFileSizeInMegaBytes(fileName)
+  if (fileSizeInMegabytes > megaBytes) {
+    fs.unlinkSync(fileName)
+  }
+}
+
+/**
+ * Log function
+ * @param {string} str Log string
+ * @param {{ prefix: string; details: string; skip: boolean }} [param1] Extra params
+ */
+export const log = (str, { prefix = '', details = '', skip } = {}) => {
+  const logPath = path.join(process.cwd(), './.log')
+  const time = new Date().toLocaleString()
+  unlinkFileIfSizeOver(logPath)
+  fs.appendFileSync(logPath, `[${time}] ${prefix}${JSON.stringify(`${str}${details && `|${details}`}`)}\n\n`)
+  if (!skip) console.log(`[${time}] ${prefix}${str}`)
+}
+
+/**
+ * Transform number-like string to hex output
+ * @param {string} str Number-like string to hex output
+ * @returns {string}
+ */
+export const toHex = (str) => str.toString(16).padStart(4, '0')
+
+/**
+ * Escape special characters
+ * @param {string} str
+ * @returns {string}
+ */
+export const escapeChars = (str) => str.replace(/_/, '\\_')
+
+/**
+ * Transform number-like to number
+ * @param {string | number} str Number-like string or number
+ * @returns number
+ */
+export const n = (str) => numeral(str).value() || 0
+
+/**
+ * Transform number-like price to formatted price
+ * @param {string | number} str Number-like string or number
+ * @returns string
+ */
+export const f = (str) => numeral(str).format(PRICE)
+
+/**
+ * @public
+ * @typedef Food
+ * @property {string} modifier
+ * @property {string} name
+ * @property {string} num
+ * @property {string} [price] Bill
+ */
+
+/**
+ * @public
+ * @typedef NormalizedFood
+ * @property {string} modifier
+ * @property {string} name
+ * @property {number} num
+ * @property {number} [price] Bill
+ */
+
+/**
+ * @public
+ * @typedef BillCustomContent
+ * @property {string} address
+ * @property {string} createdDate
+ * @property {Food[]} foodList
+ * @property {boolean} isDelivery
+ * @property {string} logo
+ * @property {string} remark
+ * @property {string} shopName
+ * @property {string} statementID
+ * @property {string} tableCode
+ * @property {string} totalPrice
+ * @property {string} [tipsFee]
+ * @property {string} [discount]
+ * @property {string} [attendant] Onsite
+ * @property {string} [customerNum] Onsite
+ * @property {string} [city] Takeway or Delivery
+ * @property {string} [province] Takeway or Delivery
+ * @property {string} [takeawayNo] Takeway
+ * @property {string} [deliveryFee] Delivery
+ * @property {string} [receiverAdress] Delivery
+ * @property {string} [receiverName] Delivery
+ * @property {string} [receiverPhone] Delivery
+ */
+
+/**
+ * @public
+ * @typedef OrderCustomContent
+ * @property {string} createdDate
+ * @property {Food} food
+ * @property {boolean} isDelivery
+ * @property {string} remark
+ * @property {string} statementID
+ * @property {string} [attendant] Onsite
+ * @property {string} [catalogId] Onsite
+ * @property {string} [tableCode] Onsite
+ * @property {string | null} [takeawayNo] Takeway or Delivery
+ * @property {string | null} [receiverName] Takeway or Delivery
+ */
+
+/**
+ * @public
+ * @typedef ToPrintBillContent
+ * @property {BillCustomContent} customerContent
+ * @property {"Network" | "USB"} hardwareType
+ * @property {string} [ip]
+ * @property {string} [vid]
+ * @property {string} [pid]
+ */
+
+/**
+ * @public
+ * @typedef ToPrintOrderContent
+ * @property {OrderCustomContent[]} chefContent
+ * @property {"Network" | "USB"} hardwareType
+ * @property {string} [ip]
+ * @property {string} [vid]
+ * @property {string} [pid]
+ */
+
+/**
+ * Build bill print content
+ * @param {BillCustomContent} billCustomContent
+ */
+export const buildBill = (billCustomContent) => {
+  const { isDelivery, takeawayNo, address, shopName, attendant, deliveryFee, tipsFee, discount, totalPrice, foodList, createdDate, statementID, remark, tableCode, receiverAdress, receiverName, receiverPhone } = billCustomContent
+
+  const isTakeaway = !!takeawayNo
+
+  const HEADER = `"^${shopName}\n\n${address}\n\n`
+
+  let SUB_HEADER = ''
+  if (!isDelivery && !isTakeaway) {
+    // Onsite - 堂食
+    SUB_HEADER = `"^TABLE ${tableCode}\n-\n`
+  } else if (isTakeaway) {
+    // Takeway - 自取
+    SUB_HEADER = `"^TAKEWAY NO. ${takeawayNo}\n-\n`
+  } else if (isDelivery) {
+    // Delivery - 外卖
+    SUB_HEADER = `"^DELIVERY\n-\n`
+  }
+
+  const deliveryFeeMd = deliveryFee ? `Delivery Fee | "^${deliveryFee}\n` : ''
+  const tipsFeeMd = tipsFee ? `^Tips | "^${tipsFee}\n` : ''
+  const discountMd = discount ? `^Discount | "^${discount}\n` : ''
+  /** @type {NormalizedFood[]} */
+  const normalizedFoodList = []
+  foodList.forEach((food) => {
+    const normalizedFood = normalizedFoodList.find((record) => record.name === food.name)
+    if (normalizedFood) {
+      normalizedFood.num += n(food.num)
+    } else {
+      normalizedFoodList.push({
+        ...food,
+        num: n(food.num),
+        price: n(food.price),
+      })
+    }
+  })
+  const FOOD_TABLE = `|Name | Qty | Price | Total|\n-
+${normalizedFoodList.map(({ name, modifier, num, price }) => `|${name} |\n${modifier ? `|[${modifier}] |\n` : ''}|| ${num} | ${f(price)} | "${f(n(num) * n(price))}|`).join('\n')}
+-\n${deliveryFeeMd}${tipsFeeMd}${discountMd}^TOTAL | "^${totalPrice}\n-\n`
+
+  const statementIDMd = statementID ? `Order No.: |${statementID}\n` : ''
+  const attendantMd = attendant ? `Attendant: |${escapeChars(attendant)}\n` : ''
+  const createdDateMd = createdDate ? `Date Time: |${createdDate}\n` : ''
+  const receiverNameMd = receiverName ? `Receiver: |${escapeChars(receiverName)}\n` : ''
+  const receiverPhoneMd = receiverPhone ? `Phone No.: |${receiverPhone}\n` : ''
+  const receiverAdressMd = receiverAdress ? `Address: |${receiverAdress}\n` : ''
+  const remarkMd = remark ? `Remark: |${remark}\n` : ''
+  const FOOTER = `{w:10,*}\n${statementIDMd}${attendantMd}${createdDateMd}${receiverNameMd}${receiverPhoneMd}${receiverAdressMd}${remarkMd}{w:auto}\n-\n\n`
+
+  return HEADER + SUB_HEADER + FOOD_TABLE + FOOTER
+}
+
+/**
+ * Build bill print content
+ * @param {OrderCustomContent} orderCustomContent
+ */
+export const buildOrder = (orderCustomContent) => {
+  const { isDelivery, takeawayNo, tableCode, food, attendant, createdDate, statementID, receiverName, remark } = orderCustomContent
+  const isTakeaway = !!takeawayNo
+
+  let SUB_HEADER = ''
+  if (!isDelivery && !isTakeaway) {
+    // Onsite - 堂食
+    SUB_HEADER = `"^TABLE ${tableCode}\n-\n`
+  } else if (isTakeaway) {
+    // Takeway - 自取
+    SUB_HEADER = `"^TAKEWAY NO. ${takeawayNo}\n-\n`
+  } else if (isDelivery) {
+    // Delivery - 外卖
+    SUB_HEADER = `"^DELIVERY\n-\n`
+  }
+
+  const FOOD_TABLE = `{w:6,*}\n|Qty |Name |\n-\n|^^^${food.num} |^^^${food.name} |${food.modifier ? `\n||^^^[${food.modifier}] |` : ''}\n{w:auto}\n-\n`
+
+  const statementIDMd = statementID ? `Order No.: |${statementID}\n` : ''
+  const attendantMd = attendant ? `Attendant: |${escapeChars(attendant)}\n` : ''
+  const createdDateMd = createdDate ? `Date Time: |${createdDate}\n` : ''
+  const receiverNameMd = receiverName ? `Receiver: |${escapeChars(receiverName)}\n` : ''
+  const remarkMd = remark ? `Remark: |${remark}\n` : ''
+  const FOOTER = `{w:10,*}\n${statementIDMd}${attendantMd}${createdDateMd}${receiverNameMd}${remarkMd}{w:auto}\n-\n`
+
+  return SUB_HEADER + FOOD_TABLE + FOOTER
+}
