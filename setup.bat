d4c290514ec641330c534c3402b2192e4f3c1952
Ynewtime <chanfoc@outlook.com>
2022-10-26 14:51:14 +0300

    minor update

--- setup.bat
+++ setup.bat
@@ -2,6 +2,7 @@
 :: Run this file only after first installation of this tool, it will install the dependencies of this tool
 :: 初次下载此工具后运行一次即可，目的是安装此工具所需的依赖
 :: 注意，内网环境需配置代理，建议断开 SPES 代理，连接公网后运行 setup
+
 chcp 65001
 @echo off
 npm i -g pnpm && pnpm i
--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 :: Waiter portal printer tools
 :: Waiter Portal 打印监听工具，需保证整个任务正常运行
+
 chcp 65001
 @echo off
 npm start
