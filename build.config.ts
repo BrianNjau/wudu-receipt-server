2b3527c4d80b7c5857af77b9eba289ab20cf1edb
Ynewtime <chanfoc@outlook.com>
2022-11-10 00:01:21 +0300

    v0.5.0-beta.1
    1. Remove pngjs since image func is not used
    2. Support minify code

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,12 @@
-export default {
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
   entries: ['./src/index'],
   clean: true,
-  declaration: true,
   rollup: {
-    emitCJS: true,
     inlineDependencies: true,
+    esbuild: {
+      minify: true,
+    },
   },
-}
+})
